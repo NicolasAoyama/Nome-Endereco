d62667654862e8db6a6063cddc9c62a6259ca57a
dogogh <quadros1919@gmail.com>
2023-07-30 19:51:27 -0300

    funcionando porem feio

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,104 +1,105 @@
 package main;
 
+import main.Endereco;
+import main.Pessoa;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Criando uma lista de pessoas e enderecos
         List<Pessoa> pessoas = new ArrayList<>();
-        //Iniciando meu scanner
         Scanner sc = new Scanner(System.in);
 
-        //Criando uma pessoa Nicolas
-        //Pessoa nicolas = new Pessoa("Nicolas",22);
-
-        //Adicionei a pessoa criada acima
-        //pessoas.add(nicolas);
-
-        //Printando o nome da pessoa
-        //System.out.println(        pessoas.get(0).getNome());
-
-        //Jeito alternativo melhor de criar uma pessoa
-        //pessoas.add(new Pessoa("Pica-Pau", 22));
-
-        //Aqui eu crio um for para insercao de 3 pessoas, o usuario inserte
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; ++i) {
             System.out.println("Digite o nome da pessoa:");
             String nome = sc.nextLine();
-
             System.out.println("Digite a idade da pessoa:");
             int idade = sc.nextInt();
-
             List<Endereco> enderecos = new ArrayList<>();
-
-            System.out.println("Quantos enderecoes essa pessoa tem?");
+            System.out.println("Quantos endereços essa pessoa tem?");
             int qtdEndereco = sc.nextInt();
             sc.nextLine();
-            for (int j=1;j<=qtdEndereco;j++){
-                System.out.println("Digite o nome e o numero do "+ j + " Endereco dessa pessoa");
+
+            for (int j = 1; j <= qtdEndereco; ++j) {
+                System.out.println("Digite o nome e o número do " + j + "º endereço dessa pessoa");
                 String enderecoNome = sc.nextLine();
                 int enderecoNumero = sc.nextInt();
                 sc.nextLine();
-                enderecos.add(new Endereco(enderecoNome,enderecoNumero));
-
+                enderecos.add(new Endereco(enderecoNome, enderecoNumero));
             }
-            pessoas.add(new Pessoa(nome,idade,enderecos));
-            //enderecos.clear();
 
+            pessoas.add(new Pessoa(nome, idade, enderecos));
+        }
 
-            System.out.println(        pessoas.get(i).getNome());
-            System.out.println(        pessoas.get(i).getIdade());
+        limparTerminal();
+        // Exibir lista
+        exibirPessoas(pessoas);
 
-            System.out.println("Enderecos:");
-            for (Endereco endereco : pessoas.get(i).getEnderecos()) {
+        // Pesquisar por nome
+        System.out.println("Digite o nome da pessoa para pesquisar:");
+        String nomePesquisado = sc.nextLine();
+        pesquisarPessoaPorNome(pessoas, nomePesquisado);
+    }
+
+    public static void exibirPessoas(List<Pessoa> pessoas) {
+        for (Pessoa pessoa : pessoas) {
+            System.out.println("Nome: " + pessoa.getNome());
+            System.out.println("Idade: " + pessoa.getIdade());
+            System.out.println("Endereços:");
+
+            List<Endereco> enderecos = pessoa.getEnderecos();
+            for (Endereco endereco : enderecos) {
                 System.out.println("Rua: " + endereco.getRua());
-                System.out.println("Numero: " + endereco.getNumero());
+                System.out.println("Número: " + endereco.getNumero());
                 System.out.println();
             }
+        }
+    }
 
+    public static void pesquisarPessoaPorNome(List<Pessoa> pessoas, String nomePesquisado) {
+        boolean encontrou = false;
+
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa.getNome().equalsIgnoreCase(nomePesquisado)) {
+                encontrou = true;
+                System.out.println("Pessoa encontrada:");
+                System.out.println("Nome: " + pessoa.getNome());
+                System.out.println("Idade: " + pessoa.getIdade());
+                System.out.println("Endereços:");
+
+                List<Endereco> enderecos = pessoa.getEnderecos();
+                for (Endereco endereco : enderecos) {
+                    System.out.println("Rua: " + endereco.getRua());
+                    System.out.println("Número: " + endereco.getNumero());
+                    System.out.println();
+                }
+                break;
+            }
+        }
+
+        if (!encontrou) {
+            System.out.println("Pessoa não encontrada.");
+        }
+    }
+
+    public static void limparTerminal() {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+            ProcessBuilder processBuilder;
+
+            if (os.contains("win")) {
+
+                processBuilder = new ProcessBuilder("cmd", "/c", "cls");
+            } else {
+
+                processBuilder = new ProcessBuilder("sh", "-c", "clear");
+            }
+
+            processBuilder.inheritIO().start().waitFor();
+        } catch (Exception e) {
 
         }
-        //Posso fazer asism tambem:
-        //for(Pessoa x: pessoas){
-        //  System.out.println(x.getNome() +" " + x.getIdade()+" Anos");
-        //}
-
-
-
-        //List<String> nomes = new ArrayList<>();
-        /* se eu quisesse fazer um generico(nao recomendado)
-        List<?>*/
-
-        //Setando nomes
-        //nomes.add("Nicolas");
-        //nomes.add("Joaozin");
-        //nomes.add("Miguelito");
-
-
-
-
-        //Printando os nomes
-        //for (int i=0;i<3;i++){
-        //System.out.println(nomes);
-        //}
-        //for (String nome : nomes){
-        //System.out.println(nomes);
-        //}
-        //Scanneando nomes
-        //for (String nome : nomes){
-        //nome = sc.nextLine();
-        //}
-        //sc.close();
-        //System.out.println(nomes.size());
-
-
-
-
-
-
-
-
     }
 }
